4560bf61a784c9a0bb21e2353fed6c9b2c275705
Shining Xu <shangnin@usc.edu>
2022-01-04 20:08:00 -0800

    Add HW 7 reset_db code

--- 7/cmupaper/paper/functions.py
+++ 7/cmupaper/paper/functions.py
@@ -80,8 +80,62 @@ def reset_db(conn):
         (0, None)   Success
         (1, None)   Failure
     """
-    return 1, None
-
+    commands = (
+        """
+        DROP TABLE IF EXISTS tags, tagnames, likes, papers, users
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS users(
+            username VARCHAR(50) NOT NULL,
+            password VARCHAR(32) NOT NULL,
+            PRIMARY KEY(username)
+        )
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS papers(
+            pid  SERIAL PRIMARY KEY,
+            username VARCHAR(50) NOT NULL,
+            title VARCHAR(50),
+            begin_time TIMESTAMP NOT NULL,
+            description VARCHAR(500),
+            data TEXT,
+            FOREIGN KEY(username) REFERENCES users ON DELETE CASCADE
+        );
+        """,
+        """
+        CREATE INDEX paper_text_idx ON papers USING gin(to_tsvector('english', data))
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS tagnames(
+            tagname VARCHAR(50) NOT NULL,
+            PRIMARY KEY(tagname)
+        );
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS likes(
+            pid INT NOT NULL,
+            username VARCHAR(50) NOT NULL,
+            like_time TIMESTAMP NOT NULL,
+            PRIMARY KEY(pid, username),
+            FOREIGN KEY(pid) REFERENCES papers ON DELETE CASCADE,
+            FOREIGN KEY(username) REFERENCES users ON DELETE CASCADE
+        );
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS tags(
+            pid INT NOT NULL,
+            tagname VARCHAR(50) NOT NULL,
+            PRIMARY KEY(pid, tagname),
+            FOREIGN KEY(pid) REFERENCES papers ON DELETE CASCADE,
+            FOREIGN KEY(tagname) REFERENCES tagnames ON DELETE CASCADE
+        );
+        """,
+    )
+    cur = conn.cursor()
+    for command in commands:
+        cur.execute(command)
+    conn.commit()
+    return 0, None
 
 # Basic APIs
 
