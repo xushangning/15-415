5240a3e10274a57fd7f0e05db3e4dc49e7bea5be
Shining Xu <shangnin@usc.edu>
2022-01-09 21:00:14 -0800

    Add explanation for the lack of reset_db test

--- 7/cmupaper/paper/tests.py
+++ 7/cmupaper/paper/tests.py
@@ -1,3 +1,8 @@
+"""Design Choices:
+1. No test for `reset_db`. The code of `reset_db` is handed down as a definition
+for database schema that shouldn't be change. What's the point of testing
+something that can't fail, because it itself is the definition of correctness?
+"""
 from django.test import TestCase
 
 # Create your tests here.
