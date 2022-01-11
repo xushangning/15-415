27361255ab25519ba35869076dd0043359a14642
Shining Xu <shangnin@usc.edu>
2022-01-10 22:35:32 -0800

    Add a test for signup

--- 7/cmupaper/paper/tests.py
+++ 7/cmupaper/paper/tests.py
@@ -4,5 +4,21 @@ for database schema that shouldn't be change. What's the point of testing
 something that can't fail, because it itself is the definition of correctness?
 """
 from django.test import TestCase
+from django.conf import settings
+import psycopg
 
-# Create your tests here.
+from paper import functions
+
+
+class DbApiTestCase(TestCase):
+
+    def setUp(self):
+        self.conn = psycopg.connect('dbname=' + settings.DATABASES['default']['NAME'])
+        functions.reset_db(self.conn)
+
+    def tearDown(self):
+        functions.reset_db(self.conn)
+        del self.conn
+
+    def test_signing_up(self):
+        self.assertEqual(functions.signup(self.conn, 'andy', 'pavlo')[0], 0)
